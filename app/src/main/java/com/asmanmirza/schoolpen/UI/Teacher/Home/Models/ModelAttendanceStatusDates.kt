6fe75d911841adef40db3d134eaf8251c1ca6dc1
shubam <subhamsingh5338@gmail.com>
2023-01-23 21:55:28 +0530

    live class started

---
+++ app/src/main/java/com/asmanmirza/schoolpen/UI/Teacher/Home/Models/ModelAttendanceStatusDates.kt
@@ -0,0 +1,3 @@
+package com.asmanmirza.schoolpen.UI.Teacher.Home.Models
+
+class ModelAttendanceStatusDates(var dateNumber:String, var fullDate:String, var present:Boolean, var holiday:Boolean, var dim:Boolean)
